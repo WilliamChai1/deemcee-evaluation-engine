9cd8ad094acb26cc6c66e1ba42aa6aff8b8c8314
WilliamChai1 <56224026+WilliamChai1@users.noreply.github.com>
2026-08-31 11:05:45 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(e) {
-  // Let network requests pass through
-  e.respondWith(fetch(e.request));
-});
